fe26819aae7e90620c4818532e6a087efad0da7b
dolphub <randy.mikkelsaar@gmail.com>
2019-08-20 10:52:02 -0400

    fix(import): Fixing auto imports WIP

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-
-import { AwesomeComponent } from '@dolphub/common-components';
-
-import { exampleService } from '@dolphub/common';
+import { exampleService } from '@dolphub/common/lib/example';
 
 const App: React.FC = () => {
   // const data = exampleService(['hello', 'world']);
-  const data = exampleService(['hello', 'world']);
+  const data = exampleService(['hello']);
 
   const onComponentClick = (val, index) => () => {
     alert(`${val}: ${index}`);
@@ -17,7 +14,7 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <strong>Data: {123}</strong>
-        <AwesomeComponent data={data} onClick={onComponentClick} />
+        {/* <AwesomeComponent data={data} onClick={onComponentClick} /> */}
       </header>
     </div>
   );
